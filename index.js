3416d590f3313bd8e20f9c11d7d0aeb410a4ce0e
zaid85j <zaid85j@gmail.com>
2019-01-08 18:31:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
-var store={drivers:[], passengers:[], trips: []
-};
+let store = { drivers: [], passengers: [], trips: [] };
 
-var driverId = null;
-var passengerId=null;
-var tripId=null;
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
 
 class Driver {
-  constructor(name){
-  this.name = name;
+  constructor(name) {
+    this.name = name;
     this.id = driverId++;
     store.drivers.push(this);
-}
-trips() {
-    return store.trips.filter( trip=> {
-      return trip.driverId == this.driverId;
+  }
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId == this.id;
     });
   }
   passengers() {
-    return this.trips().map( trip=> {
+    return this.trips().map(trip => {
       return trip.passenger();
     });
   }
 }
 
-class Passenger  {
-  constructor(name){
-  this.name = name;
+class Passenger {
+  constructor(name) {
+    this.name = name;
     this.id = passengerId++;
     store.passengers.push(this);
   }
 
-
-trips() {
-    return store.trips.filter(trip=> {
+  trips() {
+    return store.trips.filter(trip => {
       return trip.passengerId == this.id;
-          });
+    });
   }
   drivers() {
     return this.trips().map(trip => {
@@ -42,6 +40,7 @@ trips() {
     });
   }
 }
+
 class Trip {
   constructor(driver, passenger) {
     this.driverId = driver.id;
