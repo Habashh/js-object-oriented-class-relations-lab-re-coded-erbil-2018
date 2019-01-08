bb5ba13ac7805ef4a14895060d4bb729bba3546a
zaid85j <zaid85j@gmail.com>
2019-01-08 18:32:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-let store = { drivers: [], passengers: [], trips: [] };
+var store={drivers:[], passengers:[], trips: []
+};
 
-let driverId = 0;
-let passengerId = 0;
-let tripId = 0;
+var driverId = null;
+var passengerId=null;
+var tripId=null;
 
 class Driver {
-  constructor(name) {
-    this.name = name;
+  constructor(name){
+  this.name = name;
     this.id = driverId++;
     store.drivers.push(this);
-  }
-  trips() {
-    return store.trips.filter(trip => {
-      return trip.driverId == this.id;
+}
+trips() {
+    return store.trips.filter( trip=> {
+      return trip.driverId == this.driverId;
     });
   }
   passengers() {
-    return this.trips().map(trip => {
+    return this.trips().map( trip=> {
       return trip.passenger();
     });
   }
 }
 
-class Passenger {
-  constructor(name) {
-    this.name = name;
+class Passenger  {
+  constructor(name){
+  this.name = name;
     this.id = passengerId++;
     store.passengers.push(this);
   }
 
-  trips() {
-    return store.trips.filter(trip => {
+
+trips() {
+    return store.trips.filter(trip=> {
       return trip.passengerId == this.id;
-    });
+          });
   }
   drivers() {
     return this.trips().map(trip => {
@@ -40,22 +42,20 @@ class Passenger {
     });
   }
 }
-
-class Trip {
+class Trip   {
   constructor(driver, passenger) {
     this.driverId = driver.id;
     this.passengerId = passenger.id;
-    this.id = tripId++;
+      this.id = tripId++;
     store.trips.push(this);
   }
   driver() {
     return store.drivers.find(driver => {
-      return driver.id === this.driverId;
-    });
+      return driver.id == this.driverId;    });
   }
   passenger() {
-    return store.passengers.find(passenger => {
-      return passenger.id === this.passengerId;
-    });
+    return store.passengers.find( passenger => {
+      return passenger.id == this.passengerId;    });
   }
 }
+  
