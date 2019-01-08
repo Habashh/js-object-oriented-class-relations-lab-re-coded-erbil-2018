c674497733393e127f4cc65e0449fc5777c4fa64
zaid85j <zaid85j@gmail.com>
2019-01-08 17:50:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ class Passenger  {
 
 trips() {
     return store.trips.filter(function trip() {
-      return trip.passengerId == this.id;
+      return trip.passengerId === this.id;
           });
   }
   drivers() {
