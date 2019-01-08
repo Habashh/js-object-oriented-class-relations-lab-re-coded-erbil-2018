aee9e0a3d6d7d5ba467833f5f15a23ea85859bd2
zaid85j <zaid85j@gmail.com>
2019-01-08 18:13:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,11 +51,11 @@ class Trip   {
   }
   driver() {
     return store.drivers.find(driver => {
-      return driver.id == this.driverId;    });
+      return driver.id === this.driverId;    });
   }
   passenger() {
     return store.passengers.find( passenger =>{
-      return passenger.id == this.passengerId;    });
+      return passenger.id === this.passengerId;    });
   }
 }
   
