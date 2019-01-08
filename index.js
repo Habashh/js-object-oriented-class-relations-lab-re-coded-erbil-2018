9d9b18b9f8c0fb6e22293bdd5470e7fa9a1d4d06
zaid85j <zaid85j@gmail.com>
2019-01-08 17:47:56 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,60 @@
+var store={drivers:[], passengers:[], trips: []
+};
+
+var driverId = null;
+var passengerId=null;
+var tripId=null;
+
+class Driver {
+  constructor(name){
+  this.name = name;
+    this.id = driverId++;
+    store.drivers.push(this);
+}
+trips() {
+    return store.trips.filter(function trip() {
+      return trip.driverId == this.driverId;
+    });
+  }
+  passengers() {
+    return this.trips().map(function trip() {
+      return trip.passenger();
+    });
+  }
+}
+
+class Passenger  {
+  constructor(name){
+  this.name = name;
+    this.id = passengerId++;
+    store.passengers.push(this);
+  }
+
+
+trips() {
+    return store.trips.filter(function trip() {
+      return trip.passengerId == this.id;
+          });
+  }
+  drivers() {
+    return this.trips().map(function trip() {
+      return trip.driver(); });
+  }
+}
+class Trip   {
+  constructor(driver, passenger) {
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+      this.id = tripId++;
+    store.trips.push(this);
+  }
+  driver() {
+    return store.drivers.find(function driver() {
+      return driver.id == this.driverId;    });
+  }
+  passenger() {
+    return store.passengers.find(function passenger() {
+      return passenger.id == this.passengerId;    });
+  }
+}
+  
